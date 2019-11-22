efa04613fbfa10bffd131d20d2b6a4c33d208e23
David Zahn <S00930079@acad.tri-c.edu>
2019-11-21 21:41:06 -0500

    Completed lab 5

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Lab5
+{
+   
+   class Program
+   {
+      // Task 2 - Using the method getArea identify and describe 
+      //          the following elements
+
+      //  Non-Static is implicit so it is not required 
+      //                                    
+      //  access  return  method         
+      // modifier  type    name         Parameters
+      //     ↓      ↓       ↓    ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+      .    public double getArea(double height, double width)
+            {                          // The area between the curly braces
+               return height * width;  // is called the method body and is 
+            }                          // where the actual code work is done.
+
+
+      /*
+       * in the preceding method's code are the following elements:
+       * a. Scope -   Access modifiers such as 'public' and 'private' are used to limit 
+       *   (Access    which methods can call that specific method. Public can be used by
+       *   modifier)  anything and private can only be used by methods in the same class.
+       *              Some  other modifiers are 'protected' and 'internal'.
+       *            
+       * b. Static vs Non-Static - This refers to whether a method must be called as
+       *                           part of an object or not. The example method is 
+       *                           implicitly Non-static and must therefore be called
+       *                           in relation to an object.
+       * 
+       *c. Return Type - The return type is specified as 'double' and is located
+       *                  after the access modifier 'public'. This specifies that
+       *                  the method MUST return a double value when the methods 
+       *                  call ends. Some values include 'int', 'string' or 'decimal'.
+       *                  If no value was desired the term 'void' would
+       *                  be used. 
+       * 
+       * d. Method name - This method's name is 'getArea'. The method name along with
+       *                  the number and type of parameters creates a method signature
+       *                  that MUST be unique. You can create more than one method with
+       *                  the same name but it must have different parameter requirements.
+       *                  This is called 'overloading' a method.
+       * 
+       * e. Parameters - The parameters of a method are values that can be passed to it
+       *                 so that it can perform something to them. Parameters are not 
+       *                 required to be passed to it. Included in the parameters can also
+       *                 be cast modifiers to change the types as well as keywords to
+       *                 modify how the value is passed ('ref' or 'out').
+       * 
+       * 
+       * f. Method Body - This is where the actual work of the method is done. In the
+       *                  above example, the body is contained in curly brackets. 
+       *                  This isn't always required but is a good practice for readability
+       * 
+       * 
+       * 
+       * Task 3 - The difference between user-defined and framework methods:
+       *          Framework methods are "tried and true". These are methods that are 
+       *          written to do one thing generically such as math functions. They are
+       *          meant to be used in a variety of applications. User-defined methods 
+       *          are more specific to a certain project or app. Things to consider
+       *          when creating user-defined methods:
+       *          Is it trying to do too much?
+       *          Should it be broken into smaller tasks?
+       *          Is there already a method in the framework that does the same thing?
+       * 
+       * 
+       * Task 4 - Static vs. Non-Static
+       *                 Unless explicitly called Static in the method header, the method
+       *                 is considered Non-Static. A method is declared a Static method, by
+       *                 placing the keyword 'Static' between the access modifier and
+       *                 the return type. When a method is Static, it can be called by using the 
+       *                 class name, followed by the member-access operator (.) and then the
+       *                 method name. A Non-Static method must be called in relation to an
+       *                 object created by a class by using the object name, followed by the 
+       *                 member-access operator (.) and then the method name.
+       *                 
+       *                 Static method call example - Math.Pow(10,2)  ← parameters
+       *                                               ↑    ↑    
+       *                                           class  method
+       *                                           name    name
+       *                 
+       *                 Non-Static method call example - myAccount.SetName(theName)  ← parameters
+       *                                                      ↑        ↑       
+       *                                                  object     method
+       *                                                   name       name
+       *                
+      */
+
+      static void Main(string[] args)
+      {
+         Dog myDog = new Dog();
+         myDog.Name = "Fido";
+
+         // To do: uncomment the following lines of code to call the methods
+         myDog.Bark();
+         myDog.DoTrick("Fetch");
+      }
+      class Dog
+      {
+         public string Name { get; set; }
+
+         // Add bark() method
+         public void Bark()
+         {
+            Console.WriteLine("{0} is Barking...", Name);
+         }
+
+         // Add doTrick() method
+         public void DoTrick(string trickName)
+         {
+            Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", 
+               Name, trickName);
+         }
+      }
+   }
+}
---
+++ PlayingAround/PlayingAround/Program.cs
@@ -0,0 +1,139 @@
+﻿// Fig. 7.8: Craps.cs
+// Craps class simulates the dice game craps.
+using System;
+namespace PlayingAround
+{
+   class Craps
+   {
+      // create random-number generator for use in method RollDice
+      private static readonly Random randomNumbers = new Random();
+
+      // enumeration with constants that represent the game status
+      private enum Status { Continue, Won, Lost }
+
+      // enumeration with constants that represent common rolls of the dice
+      private enum DiceNames
+      {
+         SnakeEyes = 2,
+         Trey = 3,
+         Seven = 7,
+         YoLeven = 11,
+         BoxCars = 12
+      }
+
+      // plays one game of craps
+      static void Main()
+      {
+         // gameStatus can contain Continue, Won or Lost
+         Status gameStatus = Status.Continue;
+         int myPoint = 0; // point if no win or loss on first roll
+         Console.WriteLine("\n");
+         int sumOfDice = RollDice(); // first roll of the dice
+         
+
+         // determine game status and point based on first roll
+         switch ((DiceNames)sumOfDice)
+         {
+            case DiceNames.Seven: // win with 7 on first roll   
+            case DiceNames.YoLeven: // win with 11 on first roll
+               gameStatus = Status.Won;
+               break;
+            case DiceNames.SnakeEyes: // lose with 2 on first roll
+            case DiceNames.Trey: // lose with 3 on first roll     
+            case DiceNames.BoxCars: // lose with 12 on first roll 
+               gameStatus = Status.Lost;
+               break;
+            default: // did not win or lose, so remember point  
+               gameStatus = Status.Continue; // game is not over
+               myPoint = sumOfDice; // remember the point       
+               Console.WriteLine($"Point is {myPoint}");
+               break;
+         }
+
+         // while game is not complete
+         while (gameStatus == Status.Continue) // game not Won or Lost
+         {
+            Console.WriteLine("Press <ENTER> to roll again.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Point is {myPoint}\n");
+
+
+            sumOfDice = RollDice(); // roll dice again
+
+            // determine game status
+            if (sumOfDice == myPoint) // win by making point
+            {
+               gameStatus = Status.Won;
+            }
+            else
+            {
+               // lose by rolling 7 before point
+               if (sumOfDice == (int)DiceNames.Seven)
+               {
+                  gameStatus = Status.Lost;
+               }
+            }
+         }
+
+         // display won or lost message
+         if (gameStatus == Status.Won)
+         {
+            Console.WriteLine("PLAYER WINS!!!!\a");
+         }
+         else
+         {
+            Console.WriteLine("Player loses.\a");
+         }
+      }
+
+      // roll dice, calculate sum and display results
+      static int RollDice()
+      {
+         // pick random die values
+         int die1 = randomNumbers.Next(1, 7); // first die roll
+         int die2 = randomNumbers.Next(1, 7); // second die roll
+
+         int sum = die1 + die2; // sum of die values
+
+         // display results of this roll
+
+         DisplayDice(die1, die2);
+
+         return sum; // return sum of dice
+      }
+
+
+      static void DisplayDice(int die1, int die2)
+      {
+         int sum = die1 + die2;
+         /*
+         string[] dieFace1 = new string[7] { "","   ", "O  ", "O  ", "O O", "O O", "O O" };
+         string[] dieFace2 = new string[7] { ""," O ", "   ", " O ", "   ", " O ", "O O" };
+         string[] dieFace3 = new string[7] { "","   ", "  O", "  O", "O O", "O O", "O O" };
+
+         Console.WriteLine("Player rolls:");
+         Console.WriteLine("╔═══╗  ╔═══╗");
+         Console.WriteLine("║{0}║  ║{1}║", dieFace1[die1], dieFace1[die2]);
+         Console.WriteLine("║{0}║  ║{1}║   = {2}", dieFace2[die1], dieFace2[die2], sum);
+         Console.WriteLine("║{0}║  ║{1}║", dieFace3[die1], dieFace3[die2]);
+         Console.WriteLine("╚═══╝  ╚═══╝");
+         */
+         string[,] dieFace = new string[,]
+         {
+            { "   ", "O  ", "O  ", "O O", "O O", "O O" },
+            { " O ", "   ", " O ", "   ", " O ", "O O" },
+            { "   ", "  O", "  O", "O O", "O O", "O O" }
+         };
+         Console.WriteLine("Player rolls:");
+         Console.WriteLine("╔═══╗  ╔═══╗");
+         for (int i=0;i<=2;i++)
+         {
+            Console.WriteLine("║{0}║  ║{1}║", dieFace[i,die1-1], dieFace[i,die2-1]);
+         }
+         Console.WriteLine("╚═══╝  ╚═══╝   =  {0}",sum);
+      }
+
+   }
+
+}
