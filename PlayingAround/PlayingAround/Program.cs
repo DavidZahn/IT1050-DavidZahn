f8bce5e9ea88aba6863457dac636df84043c38f6
David Zahn <S00930079@acad.tri-c.edu>
2019-11-22 15:07:12 -0500

    Just playing around

--- PlayingAround/PlayingAround/Program.cs
+++ PlayingAround/PlayingAround/Program.cs
@@ -24,67 +24,109 @@ namespace PlayingAround
       // plays one game of craps
       static void Main()
       {
-         // gameStatus can contain Continue, Won or Lost
-         Status gameStatus = Status.Continue;
-         int myPoint = 0; // point if no win or loss on first roll
-         Console.WriteLine("\n");
-         int sumOfDice = RollDice(); // first roll of the dice
-         
+         int bank = 1000;
 
-         // determine game status and point based on first roll
-         switch ((DiceNames)sumOfDice)
-         {
-            case DiceNames.Seven: // win with 7 on first roll   
-            case DiceNames.YoLeven: // win with 11 on first roll
-               gameStatus = Status.Won;
-               break;
-            case DiceNames.SnakeEyes: // lose with 2 on first roll
-            case DiceNames.Trey: // lose with 3 on first roll     
-            case DiceNames.BoxCars: // lose with 12 on first roll 
-               gameStatus = Status.Lost;
-               break;
-            default: // did not win or lose, so remember point  
-               gameStatus = Status.Continue; // game is not over
-               myPoint = sumOfDice; // remember the point       
-               Console.WriteLine($"Point is {myPoint}");
-               break;
-         }
+         Console.WriteLine($"Your bank is {bank}.\nEnter your bet (0 to quit):");
+         int bet = int.Parse(Console.ReadLine());
 
-         // while game is not complete
-         while (gameStatus == Status.Continue) // game not Won or Lost
+         while (bet>0 && bank>0)
          {
-            Console.WriteLine("Press <ENTER> to roll again.");
-            Console.ReadLine();
             Console.Clear();
-            Console.WriteLine($"Point is {myPoint}\n");
+            Console.WriteLine("Bank:{0}  Bet:{1}", bank, bet);
+            // gameStatus can contain Continue, Won or Lost
+            Status gameStatus = Status.Continue;
+            int myPoint = 0; // point if no win or loss on first roll
+            Console.WriteLine("\n");
+            int sumOfDice = RollDice(); // first roll of the dice
 
 
-            sumOfDice = RollDice(); // roll dice again
-
-            // determine game status
-            if (sumOfDice == myPoint) // win by making point
+            // determine game status and point based on first roll
+            switch ((DiceNames)sumOfDice)
             {
-               gameStatus = Status.Won;
+               case DiceNames.Seven: // win with 7 on first roll   
+               case DiceNames.YoLeven: // win with 11 on first roll
+                  gameStatus = Status.Won;
+                  break;
+               case DiceNames.SnakeEyes: // lose with 2 on first roll
+               case DiceNames.Trey: // lose with 3 on first roll     
+               case DiceNames.BoxCars: // lose with 12 on first roll 
+                  gameStatus = Status.Lost;
+                  break;
+               default: // did not win or lose, so remember point  
+                  gameStatus = Status.Continue; // game is not over
+                  myPoint = sumOfDice; // remember the point       
+                  Console.WriteLine($"Point is {myPoint}");
+                  break;
+            }
+
+            // while game is not complete
+            while (gameStatus == Status.Continue) // game not Won or Lost
+            {
+               Console.WriteLine("Press <ENTER> to roll again.");
+               Console.ReadLine();
+               Console.Clear();
+               Console.WriteLine("Bank:{0}  Bet:{1}", bank, bet);
+               Console.WriteLine($"Point is {myPoint}\n");
+
+
+               sumOfDice = RollDice(); // roll dice again
+
+               // determine game status
+               if (sumOfDice == myPoint) // win by making point
+               {
+                  gameStatus = Status.Won;
+               }
+               else
+               {
+                  // lose by rolling 7 before point
+                  if (sumOfDice == (int)DiceNames.Seven)
+                  {
+                     gameStatus = Status.Lost;
+                  }
+               }
+            }
+
+            // display won or lost message
+            if (gameStatus == Status.Won)
+            {
+               Console.WriteLine("PLAYER WINS!!!!\n\a");
+               bank += bet;
             }
             else
             {
-               // lose by rolling 7 before point
-               if (sumOfDice == (int)DiceNames.Seven)
+               Console.WriteLine("Player loses.\n\a");
+               bank -= bet;
+            }
+
+            Console.WriteLine($"Your bank is {bank}.");
+            if (bank <= 0)
+            {
+               Console.WriteLine("You are BUST.");
+            }
+            else
+            {
+               Console.WriteLine("Enter your bet (0 to quit):");
+               string input = Console.ReadLine();
+               if (input == "")
                {
-                  gameStatus = Status.Lost;
+                  bet = 0;
+               }
+               else
+               {
+                  bet = int.Parse(input);
                }
             }
          }
-
-         // display won or lost message
-         if (gameStatus == Status.Won)
+         Console.Write("\n\nGit outta here");
+         if (bank <= 0)
          {
-            Console.WriteLine("PLAYER WINS!!!!\a");
+            Console.WriteLine(", ya bum!");
          }
          else
          {
-            Console.WriteLine("Player loses.\a");
+            Console.WriteLine("!");
          }
+
       }
 
       // roll dice, calculate sum and display results
@@ -107,18 +149,7 @@ namespace PlayingAround
       static void DisplayDice(int die1, int die2)
       {
          int sum = die1 + die2;
-         /*
-         string[] dieFace1 = new string[7] { "","   ", "O  ", "O  ", "O O", "O O", "O O" };
-         string[] dieFace2 = new string[7] { ""," O ", "   ", " O ", "   ", " O ", "O O" };
-         string[] dieFace3 = new string[7] { "","   ", "  O", "  O", "O O", "O O", "O O" };
-
-         Console.WriteLine("Player rolls:");
-         Console.WriteLine("╔═══╗  ╔═══╗");
-         Console.WriteLine("║{0}║  ║{1}║", dieFace1[die1], dieFace1[die2]);
-         Console.WriteLine("║{0}║  ║{1}║   = {2}", dieFace2[die1], dieFace2[die2], sum);
-         Console.WriteLine("║{0}║  ║{1}║", dieFace3[die1], dieFace3[die2]);
-         Console.WriteLine("╚═══╝  ╚═══╝");
-         */
+         
          string[,] dieFace = new string[,]
          {
             { "   ", "O  ", "O  ", "O O", "O O", "O O" },
@@ -126,12 +157,12 @@ namespace PlayingAround
             { "   ", "  O", "  O", "O O", "O O", "O O" }
          };
          Console.WriteLine("Player rolls:");
-         Console.WriteLine("╔═══╗  ╔═══╗");
+         Console.WriteLine("╔═════╗  ╔═════╗");
          for (int i=0;i<=2;i++)
          {
-            Console.WriteLine("║{0}║  ║{1}║", dieFace[i,die1-1], dieFace[i,die2-1]);
+            Console.WriteLine("║ {0} ║  ║ {1} ║", dieFace[i,die1-1], dieFace[i,die2-1]);
          }
-         Console.WriteLine("╚═══╝  ╚═══╝   =  {0}",sum);
+         Console.WriteLine("╚═════╝  ╚═════╝   =  {0}", sum);
       }
 
    }
